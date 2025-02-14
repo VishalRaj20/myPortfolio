00289764387194741af575842bc3ad5dec28f71b
Vishal Raj <126649319+VishalRaj20@users.noreply.github.com>
2025-02-14 23:34:47 +0530

    Update Home2.js

--- src/Home2.js
+++ src/Home2.js
@@ -20,32 +20,38 @@ function Home2() {
               LET ME <span className="purple"> INTRODUCE </span> MYSELF
             </h1>
             <p className="home-about-body">
-              I fell in love with programming and I have at least learnt
-              something, I think… 🤷‍♂️
+              I fell in love with programming, and I have learned
+              quite a lot, I believe… 🤷‍♂️
               <br />
               <br />I am fluent in classics like
               <i>
-                <b className="purple"> C++, Javascript and Go. </b>
+                <b className="purple"> C++, Python, and Java. </b>
               </i>
               <br />
               <br />
-              My field of Interest's are building new &nbsp;
+              I specialize in <b className="purple">Web Development</b>, creating dynamic and responsive web applications.
+              <br />
+              <br />
+              I am also <b className="purple">proficient in Data Structures and Algorithms (DSA) in C++</b>, solving complex problems efficiently.
+              <br />
+              <br />
+              My field of interest includes building new &nbsp;
               <i>
-                <b className="purple">Web Technologies and Products </b>
+                <b className="purple">Web Technologies and Products. </b>
               </i>
               <br />
               <br />
               Whenever possible, I also apply my passion for developing products
-              with <b className="purple">Node.js</b> and
+              using <b className="purple">Node.js</b> and
               <i>
                 <b className="purple">
                   {" "}
-                  Modern Javascript Library and Frameworks
+                  modern JavaScript libraries and frameworks
                 </b>
               </i>
               &nbsp; like
               <i>
-                <b className="purple"> React.js and Next.js</b>
+                <b className="purple"> React.js and Next.js.</b>
               </i>
             </p>
           </Col>
@@ -59,7 +65,7 @@ function Home2() {
           <Col md={12} className="home-about-social">
             <h1>FIND ME ON</h1>
             <p>
-              Feel free to <span className="purple">connect </span>with me
+              Feel free to <span className="purple">connect </span>with me.
             </p>
             <ul className="home-about-social-links">
               <li className="social-icons">
@@ -84,7 +90,7 @@ function Home2() {
               </li>
               <li className="social-icons">
                 <a
-                  href="ttps://www.linkedin.com/in/vishal-raj-816485253/"
+                  href="https://www.linkedin.com/in/vishal-raj-816485253/"
                   target="_blank"
                   rel="noreferrer"
                   className="icon-colour  home-social-icons"
@@ -109,4 +115,6 @@ function Home2() {
     </Container>
   );
 }
+
+
 export default Home2;
